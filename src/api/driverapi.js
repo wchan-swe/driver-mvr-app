025a2ac21179212a669285afc08d2a8911d768ac
Wayne Chan <wchan.it@outlook.com>
2022-01-14 15:15:03 -0600

    Add prod url

--- src/api/driverapi.js
+++ src/api/driverapi.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:8080/api/v1',
+  // baseURL: 'http://localhost:8080/api/v1',
+  baseURL: 'https://driver-mvr-app.herokuapp.com/api/v1',
 });
